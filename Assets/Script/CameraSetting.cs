785f82d43a1a24d523c4da8c583e6debec8b1b6b
Muhammad Ihsan Diputra <isan.only@gmail.com>
2019-10-30 19:20:01 +0800

    Add Material

---
+++ Assets/Script/CameraSetting.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSetting : MonoBehaviour
+{
+
+    public GameObject point;
+    public float minRadius = 5;
+    public float maxRadius = 25;
+    int radius = 0;
+    int iteration = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float minRadius = this.minRadius;
+        float radius = minRadius;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (radius >= maxRadius)
+        {
+            Application.Quit();
+            UnityEditor.EditorApplication.isPlaying = false;
+            return;
+            
+        }
+
+        float x = Random.Range((float)-radius / 2, (float)radius / 2);
+        float y = Random.Range(1, (float)radius / 2);
+        float z = Random.Range((float)-radius / 2, (float)radius / 2);
+        this.transform.position = new Vector3(point.transform.position.x + x, point.transform.position.y + y, 
+            point.transform.position.z + z);
+        this.transform.LookAt(point.transform.position);
+            
+        iteration++;
+        if (iteration > 10)
+        {
+            iteration = 0;
+            radius++;
+        }
+    }
+}
---
+++ Assets/Script/Screenshot.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System;
+
+public class Screenshot : MonoBehaviour
+{
+    public string FolderName = "ScreenShot Folder SOA";
+    public string FileName = "Screenshot";
+    public int frameRate = 24;
+    public bool isAlwaysCapture = false;
+    // Use this for initialization
+    void Start () {
+        // Set the playback framerate (real time will not relate to game time after this).
+        Time.captureFramerate = frameRate;
+
+        // Create the folder
+        //System.IO.Directory.CreateDirectory(folder);
+    }
+
+    // Update is called once per frame
+    void Update() {       
+
+        switch (isAlwaysCapture) {
+            case false:
+
+                if (Input.GetKeyDown(KeyCode.F9))
+                    ScreenCaptureFunction();
+                break;
+            case true:
+                ScreenCaptureFunction();
+                break;
+        }         
+
+	}
+
+    public void ScreenCaptureFunction()
+    {
+        string path = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures) + "/" + FolderName + "/";
+        if (!Directory.Exists(path))
+            Directory.CreateDirectory(path);
+        int i = 0;
+        string pathFinal = "";
+        do
+        {
+            i++;
+            pathFinal = path + FileName+ (i < 10 ? "000" : i < 100 ? "00" : i < 1000 ? "0" : "") + i + ".png";
+        }
+        while (File.Exists(pathFinal));
+        ScreenCapture.CaptureScreenshot(pathFinal);
+    }
+    /*
+    // The folder to contain our screenshots.
+    // If the folder exists we will append numbers to create an empty folder.
+    public string folder = "ScreenshotFolder";
+    public int frameRate = 25;
+    void Start()
+    {
+        // Set the playback framerate (real time will not relate to game time after this).
+        Time.captureFramerate = frameRate;
+
+        // Create the folder
+        System.IO.Directory.CreateDirectory(folder);
+    }
+
+    void Update()
+    {
+        // Append filename to folder name (format is '0005 shot.png"')
+        string name = string.Format("{0}/{1:D04} shot.png", folder, Time.frameCount);
+
+        // Capture the screenshot to the specified file.
+        ScreenCapture.CaptureScreenshot(name);
+    }
+    */
+}
