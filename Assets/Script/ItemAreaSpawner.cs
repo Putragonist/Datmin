9320f11819b716f1e14efdfcd6d6763198b51c8a
YONI AZHAR <yoni.azhar@gmail.com>
2019-10-30 22:44:22 +0700

    Spawner Object
    Aku buat baru di dalam spawner scene

---
+++ Assets/Script/ItemAreaSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemAreaSpawner : MonoBehaviour
+{
+    public GameObject itemToSpread;
+
+    int numItemToSpawn = 30;
+
+    public float itemXSpread = 10;
+    public float itemYSpread = 0;
+    public float itemZSpread = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < numItemToSpawn; i++)
+        {
+            SpreadItems();
+        }
+    }
+
+    void SpreadItems()
+    {
+        Vector3 randomPostion = new Vector3(Random.Range(-itemXSpread, itemXSpread), Random.Range(-itemYSpread, itemYSpread), Random.Range(-itemZSpread, itemZSpread)) + transform.position;
+        GameObject clone = Instantiate(itemToSpread, randomPostion, Quaternion.identity);
+    }
+
+   
+}
---
+++ Assets/Script/PickItem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickItem : MonoBehaviour
+{
+    public GameObject[] itemToPickFrom;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Pick();
+    }
+
+    void Pick()
+    {
+        int randomIndex = Random.Range(0, itemToPickFrom.Length);
+        GameObject clone = Instantiate(itemToPickFrom[randomIndex], transform.position, Quaternion.identity);
+
+    }
+
+}
